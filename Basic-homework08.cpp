c241d69b73a30106a70142157d07b12e0e225d7f
XiaoHsiang <48340410+XiaoHsiang@users.noreply.github.com>
2019-03-29 10:38:56 +0800

    Add files via upload

---
+++ Basic-homework08.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	int factor=2;
+	int n;
+	printf("1-n質數，請輸入n:");
+	scanf("%d、",&n);
+	for(int i=2;i<=n;i++){
+		if(i==2){
+			printf("%d",i);
+			printf(" ");
+			continue;
+		}
+		while(i%factor!=0&&factor<=i){
+			factor+=1;
+			if(factor/i==1){
+				printf("%d",i);
+				printf(" ");
+				factor=2;
+				break;
+			} 
+			if(i%factor==0){
+				factor=2;
+				break;
+			}	
+		}
+	}
+	system("pause");
+	return 0;
+}
